905db6ebadbae53257701692756115697a7df980
zhengao261 <583428134@qq.com>
2017-01-16 15:20:26 +0800

    添加属性备注

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,15 @@ const eslintrc = {
     'babel'
   ],
   rules: {
-    
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': 0,
+    'no-extraneous-dependencies': 0,
+    'linebreak-style': 0,
+    'no-plusplus': 0,
+    'prefer-template': 0,
+    'max-len': 0,
+    'no-unused-expressions': 0,
+    'import/extensions': 0
   }
 };
 
